886a13b52cc4b8be12221a55ddecea4becdbd795
Kumar24, Rahul <rahul_kumar24@dell.com>
2020-09-14 18:52:08 +0530

    Test

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerApi.Model;
+using CustomerApi.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerRepository _customerRepository;
+        [HttpGet]
+        public ActionResult<List<Customer>> Get()
+        {
+            return _customerRepository.GetCustomer();
+        }
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+    }
+}
---
+++ CustomerApi/Repository/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerApi.Model;
+
+namespace CustomerApi.Repository
+{
+    public interface ICustomerRepository
+    {
+        List<Customer> GetCustomer();
+    }
+    public class CustomerRepository : ICustomerRepository
+    {
+        public List<Customer> GetCustomer()
+        {
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Id = 1, FirstName = "FC1", LastName = "LC1", Age = 20 });
+            customers.Add(new Customer { Id = 2, FirstName = "FC2", LastName = "LC2", Age = 50 });
+            return customers;
+        }
+    }
+}
